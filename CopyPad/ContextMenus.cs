238b181bdfa3169308a87fa68d59cff0143c239c
Brad Mason <DeadlyBrad42@gmail.com>
2012-06-09 01:54:57 -0400

    Initial commit

---
+++ CopyPad/ContextMenus.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+using System.Windows.Forms;
+using CopyPad.Properties;
+using System.Drawing;
+
+namespace CopyPad
+{
+	class ContextMenus
+	{
+		private static List<String> emoticons;
+		public static int lastClicked = 0;
+
+		public ContextMenu Create()
+		{
+			ContextMenu menu = new ContextMenu();
+			MenuItem item;
+
+			emoticons = new List<string>();
+			emoticons.Add(@"(⌐■_■)");
+			emoticons.Add(@"('x_x)G-(._.Q)");
+			emoticons.Add(@"ಠ_ಠ");
+			emoticons.Add(@"( ¬‿¬)");
+			emoticons.Add(@"(◕‿◕✿)");
+			emoticons.Add(@"(◑‿◐)");
+			emoticons.Add(@"(｡♥‿♥｡)");
+			emoticons.Add(@"ლ(́◉◞౪◟◉‵ლ)");
+			emoticons.Add(@"(ﾉ◕ヮ◕)ﾉ");
+			emoticons.Add(@"(✖╭╮✖)");
+			emoticons.Add(@"(。・_・。)");
+			emoticons.Add(@"(⊙_◎)");
+			emoticons.Add(@"(ノಠ益ಠ)ノ彡┻━┻");
+			emoticons.Add(@"┬─┬ノ( º _ ºノ)");
+			emoticons.Add(@"☉‿☉");
+			emoticons.Add(@"╭∩╮（︶︿︶）╭∩╮");
+			emoticons.Add(@"⊙﹏⊙");
+			emoticons.Add(@"✲ﾟ｡.(✿╹◡╹)ﾉ☆.｡₀:*ﾟ✲");
+			emoticons.Add(@"◕‿◕");
+
+			foreach (String emoticon in emoticons)
+			{
+				String thisEmoticon = emoticon;
+
+				item = new MenuItem();
+				item.Text = thisEmoticon;
+				item.Click += delegate
+				{
+					Clipboard.SetText(thisEmoticon);
+				};
+
+				menu.MenuItems.Add(item);
+			}
+
+			return menu;
+		}
+	}
+}
---
+++ CopyPad/ProcessIcon.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CopyPad
+{
+	class ProcessIcon : IDisposable
+	{
+		NotifyIcon tray;
+
+		public ProcessIcon()
+		{
+			tray = new NotifyIcon();
+		}
+
+		public void Display()
+		{
+			tray.MouseClick += new MouseEventHandler(click);
+			tray.Icon = Properties.Resources.noticon;
+			tray.Text = "CopyPad";
+			tray.Visible = true;
+
+			tray.ContextMenu = (new ContextMenus()).Create();
+		}
+
+		public void Dispose()
+		{
+			tray.Dispose();
+		}
+
+		void click(object sender, EventArgs e)
+		{
+			//tray.ShowBalloonTip(1000, "CopyPad for Windows!", "Programmed for no real reason by Brad Mason", ToolTipIcon.Info);
+			Clipboard.SetText("ಠ_ಠ");
+		}
+	}
+}
